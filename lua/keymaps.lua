b781134030291c7788cb581eb9959ab0c96efde9
Johan Frisk <johan@friskenstein.com>
2025-02-03 13:04:39 +0200

    WIP configure which key keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,6 +19,9 @@ if vim.g.neovide then
 	end)
 
 	vim.g.neovide_input_macos_option_key_is_meta = 'both'
+
+	vim.g.neovide_cursor_smooth_blink = true
+	vim.opt.guicursor = "n-v:block-blinkwait500-blinkon600-blinkoff400,i-c:ver25-blinkwait500-blinkon600-blinkoff400"
 end
 
 vim.keymap.set('v', 'i,', '<esc>T,vt,', {desc='inside commas'})
@@ -63,12 +66,15 @@ end, { desc = 'Find File' })
 --   ["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
 --   ["h"] = { "<cmd>nohlsearch<CR>", "No Highlight" },
 
-require('which-key').register({
-	s = {
-		name = "Telescope [S]earch ",
-		b = { "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find({ sorter = require('telescope.sorters').get_substr_matcher({})})<cr>", "Grep current file" },
-		B = { "<cmd>Telescope git_branches<cr>", "Git branches" },
-		c = { function()
+
+require("which-key").add({
+
+
+	{ '<leader>s', group = 'Telescope [S]earch ' },
+
+	{ "<leader>sb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find({ sorter = require('telescope.sorters').get_substr_matcher({})})<cr>", desc = "Grep current file" },
+	{ "<leader>sB", "<cmd>Telescope git_branches<cr>", desc = "Git branches" },
+	{ "<leader>sc", function()
 			local builtins = { "zellner", "torte", "slate", "shine", "ron", "quiet", "peachpuff", "habamax",
 				"pablo", "murphy", "lunaperche", "koehler", "industry", "evening", "elflord", "morning",
 				"desert", "delek", "default", "darkblue", "blue", "vim", "sorbet", "wildcharm", "zaibatsu", "retrobox" }
@@ -84,207 +90,230 @@ require('which-key').register({
 			-- require("lazyvim.util").telescope("colorscheme", { enable_preview = true })()
 			require('telescope.builtin').colorscheme {enable_preview = true}
 			vim.fn.getcompletion = target
-		end, "Colorschemes" },
-		f = { "<cmd>Telescope find_files<cr>", "Files" },
-		H = { "<cmd>Telescope help_tags<cr>", "Help tags" },
-		h = { "<cmd>Telescope highlights<cr>", "Highlight groups" },
-		M = { "<cmd>Telescope man_pages<cr>", "Man pages" },
-		-- n = { "<cmd>Telescope notify<cr>", "Notifications" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
-		R = { "<cmd>Telescope registers<cr>", "Registers" },
-		t = { "<cmd>Telescope live_grep<cr>", "Text Grep" },
-		g = { '<cmd>LiveGrepGitRoot<cr>', 'Grep on Git Root' },
-		w = { require('telescope.builtin').grep_string, 'Grep by current Word' },
-		d = { require('telescope.builtin').diagnostics, 'Diagnostics' },
-		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-		C = { "<cmd>Telescope commands<cr>", "Commands" },
-		l = { "<cmd>Telescope resume<cr>", "Resume last search" },
-		s = { "<cmd>Telescope<cr>", "Select Telescope" },
-	},
-	a = { "<cmd>AerialToggle<cr>", "Aerial" },
-	A = {
-		name = "AI copilot",
-		A = { "<cmd>CodeiumAuto<cr>", "Auto" },
-		M = { "<cmd>CodeiumManual<cr>", "Manual" },
-		-- TODO: needs to be insert mode keymapping
-		--a = { function() vim.api.nvim_call_function("codeium#Complete", {}) end, "Suggest" },
-		e = { "<cmd>CodeiumEnable<cr>", "Enable" },
-		d = { "<cmd>CodeiumDisable<cr>", "Disable" },
-		a = { "<cmd>CodeiumToggle<cr>", "Toggle" },
-	},
-	T = { ":tabe<cr>", "New Tab" },
-	t = {
-		name = "TODO & Terminal",
-		f = { "<cmd>ToggleTerm direction=float<cr>", "Floating Terminal" },
-		h = { "<cmd>ToggleTerm direction=horizontal<cr>", "Horizontal Terminal" },
-		t = { "<cmd>TodoTelescope<cr>", "Todo Telescope" },
-		l = { "<cmd>TodoLocList<cr>", "Todo LocationList" },
-		q = { "<cmd>TodoQuickFix<cr>", "Todo QuickFix" },
-		r = { "<cmd>TodoTrouble<cr>", "Todo Trouble" },
-	},
-	C = {
-		name = "CSV",
-		C = { "<cmd>%UnArrangeColumn|%ArrangeColumn!<cr>", "Refresh" },
-		a = { "<cmd>%ArrangeColumn!<cr>", "Arrange" },
-		u = { "<cmd>%UnArrangeColumn<cr>", "Un-arrange" },
-		l = { "<cmd>let b:csv_arrange_align='l*'|%ArrangeColumn!<cr>", "Align Left" },
-		r = { "<cmd>let b:csv_arrange_align='r*'|%ArrangeColumn!<cr>", "Align Right" },
-		c = { "<cmd>let b:csv_arrange_align='c*'|%ArrangeColumn!<cr>", "Align Center" },
-		d = { "<cmd>let b:csv_arrange_align='.*'|%ArrangeColumn!<cr>", "Align Decimal" },
-	},
-	F = {
-		name = "Format",
-		i = { "<Esc>gg=G<CR>", "Reindent buffer" },
-		F = { "<cmd>!noglob npx prettier % --write<CR>", "Prettier" },
-		e = { "<cmd>!noglob eslint_d --fix %<CR>", "ESLint" },
-		H = { "<cmd>%!xxd<cr>", "Filter to HEX" },
-		h = { "<cmd>%!xxd -r<cr>", "HEX -> text" },
-	},
-	g = {
-		name = 'Git',
+		end, desc = "Colorschemes" },
 
-		--INFO: already <leader>f
-		--f = { require 'telescope.builtin'.git_files , 'Search Git Files' },
+-- 	s = {
+-- 		name = "Telescope [S]earch ",
+-- 		b = { "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find({ sorter = require('telescope.sorters').get_substr_matcher({})})<cr>", "Grep current file" },
+-- 		B = { "<cmd>Telescope git_branches<cr>", "Git branches" },
+-- 		c = { function()
+-- 			local builtins = { "zellner", "torte", "slate", "shine", "ron", "quiet", "peachpuff", "habamax",
+-- 				"pablo", "murphy", "lunaperche", "koehler", "industry", "evening", "elflord", "morning",
+-- 				"desert", "delek", "default", "darkblue", "blue", "vim", "sorbet", "wildcharm", "zaibatsu", "retrobox" }
+-- 			local target = vim.fn.getcompletion
+--
+-- 			---@diagnostic disable-next-line: duplicate-set-field
+-- 			vim.fn.getcompletion = function()
+-- 				return vim.tbl_filter(function(color)
+-- 					return not vim.tbl_contains(builtins, color)
+-- 				end, target("", "color"))
+-- 			end
+--
+-- 			-- require("lazyvim.util").telescope("colorscheme", { enable_preview = true })()
+-- 			require('telescope.builtin').colorscheme {enable_preview = true}
+-- 			vim.fn.getcompletion = target
+-- 		end, "Colorschemes" },
+-- 		f = { "<cmd>Telescope find_files<cr>", "Files" },
+-- 		H = { "<cmd>Telescope help_tags<cr>", "Help tags" },
+-- 		h = { "<cmd>Telescope highlights<cr>", "Highlight groups" },
+-- 		M = { "<cmd>Telescope man_pages<cr>", "Man pages" },
+-- 		-- n = { "<cmd>Telescope notify<cr>", "Notifications" },
+-- 		r = { "<cmd>Telescope oldfiles<cr>", "Recent files" },
+-- 		R = { "<cmd>Telescope registers<cr>", "Registers" },
+-- 		t = { "<cmd>Telescope live_grep<cr>", "Text Grep" },
+-- 		g = { '<cmd>LiveGrepGitRoot<cr>', 'Grep on Git Root' },
+-- 		w = { require('telescope.builtin').grep_string, 'Grep by current Word' },
+-- 		d = { require('telescope.builtin').diagnostics, 'Diagnostics' },
+-- 		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+-- 		C = { "<cmd>Telescope commands<cr>", "Commands" },
+-- 		l = { "<cmd>Telescope resume<cr>", "Resume last search" },
+-- 		s = { "<cmd>Telescope<cr>", "Select Telescope" },
+-- 	},
+-- 	a = { "<cmd>AerialToggle<cr>", "Aerial" },
+-- 	A = {
+-- 		name = "AI copilot",
+-- 		A = { "<cmd>CodeiumAuto<cr>", "Auto" },
+-- 		M = { "<cmd>CodeiumManual<cr>", "Manual" },
+-- 		-- TODO: needs to be insert mode keymapping
+-- 		--a = { function() vim.api.nvim_call_function("codeium#Complete", {}) end, "Suggest" },
+-- 		e = { "<cmd>CodeiumEnable<cr>", "Enable" },
+-- 		d = { "<cmd>CodeiumDisable<cr>", "Disable" },
+-- 		a = { "<cmd>CodeiumToggle<cr>", "Toggle" },
+-- 	},
+	T = {"<leader>T", ":tabe<cr>", desc="New Tab" },
+-- 	t = {
+-- 		name = "TODO & Terminal",
+-- 		f = { "<cmd>ToggleTerm direction=float<cr>", "Floating Terminal" },
+-- 		h = { "<cmd>ToggleTerm direction=horizontal<cr>", "Horizontal Terminal" },
+-- 		t = { "<cmd>TodoTelescope<cr>", "Todo Telescope" },
+-- 		l = { "<cmd>TodoLocList<cr>", "Todo LocationList" },
+-- 		q = { "<cmd>TodoQuickFix<cr>", "Todo QuickFix" },
+-- 		r = { "<cmd>TodoTrouble<cr>", "Todo Trouble" },
+-- 	},
+	{"<leader>C", group="CSV"},
+		{"<leader>CC", "<cmd>%UnArrangeColumn|%ArrangeColumn!<cr>", desc="Refresh" },
+		{"<leader>Ca", "<cmd>%ArrangeColumn!<cr>", desc="Arrange" },
+		{"<leader>Cu", "<cmd>%UnArrangeColumn<cr>", desc="Un-arrange" },
+		{"<leader>Cl", "<cmd>let b:csv_arrange_align='l*'|%ArrangeColumn!<cr>", desc="Align Left" },
+		{"<leader>Cr", "<cmd>let b:csv_arrange_align='r*'|%ArrangeColumn!<cr>", desc="Align Right" },
+		{"<leader>Cc", "<cmd>let b:csv_arrange_align='c*'|%ArrangeColumn!<cr>", desc="Align Center" },
+		{"<leader>Cd", "<cmd>let b:csv_arrange_align='.*'|%ArrangeColumn!<cr>", desc="Align Decimal" },
+-- 	},
+-- 	F = {
+-- 		name = "Format",
+-- 		i = { "<Esc>gg=G<CR>", "Reindent buffer" },
+-- 		F = { "<cmd>!noglob npx prettier % --write<CR>", "Prettier" },
+-- 		e = { "<cmd>!noglob eslint_d --fix %<CR>", "ESLint" },
+-- 		H = { "<cmd>%!xxd<cr>", "Filter to HEX" },
+-- 		h = { "<cmd>%!xxd -r<cr>", "HEX -> text" },
+-- 	},
+-- 	g = {
+-- 		name = 'Git',
+--
+-- 		--INFO: already <leader>f
+-- 		--f = { require 'telescope.builtin'.git_files , 'Search Git Files' },
+--
+-- 		--INFO: see ./plugins/gitsigns.lua for more
+-- 		j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
+-- 		k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+--
+-- 		--TODO: HEAD or index or ~?
+-- 		--d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Git Diff", },
+--
+-- 		g = {
+-- 			function()
+-- 				local Terminal = require("toggleterm.terminal").Terminal
+-- 				local lazygit = Terminal:new {
+-- 					cmd = "lazygit",
+-- 					hidden = true,
+-- 					direction = "float",
+-- 					float_opts = {
+-- 						border = "none",
+-- 						width = vim.o.columns,
+-- 						height = vim.o.lines,
+-- 					},
+-- 					on_open = function(_)
+-- 						vim.cmd "startinsert!"
+-- 					end,
+-- 					on_close = function(_) end,
+-- 					count = 99,
+-- 				}
+-- 				lazygit:toggle()
+-- 			end
+-- 			, "Lazygit" },
+--
+-- 		o = { "<cmd>Telescope git_status<cr>", " Git status (N/A)" },
+-- 		B = { "<cmd>Telescope git_branches<cr>", " branches" },
+-- 		c = { "<cmd>Telescope git_commits<cr>", " commits" },
+-- 		C = { "<cmd>Telescope git_bcommits<cr>", " commits (current file)", },
+-- 	},
 
-		--INFO: see ./plugins/gitsigns.lua for more
-		j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
-		k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
+	{"<leader>u", group="UI"},
 
-		--TODO: HEAD or index or ~?
-		--d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Git Diff", },
-
-		g = {
-			function()
-				local Terminal = require("toggleterm.terminal").Terminal
-				local lazygit = Terminal:new {
-					cmd = "lazygit",
-					hidden = true,
-					direction = "float",
-					float_opts = {
-						border = "none",
-						width = vim.o.columns,
-						height = vim.o.lines,
-					},
-					on_open = function(_)
-						vim.cmd "startinsert!"
-					end,
-					on_close = function(_) end,
-					count = 99,
-				}
-				lazygit:toggle()
-			end
-			, "Lazygit" },
-
-		o = { "<cmd>Telescope git_status<cr>", " Git status (N/A)" },
-		B = { "<cmd>Telescope git_branches<cr>", " branches" },
-		c = { "<cmd>Telescope git_commits<cr>", " commits" },
-		C = { "<cmd>Telescope git_bcommits<cr>", " commits (current file)", },
-	},
-	u = {
-		name = "UI",
-		x = { "<cmd>set tabstop=2<cr><cmd>set shiftwidth=0<cr><cmd>set noet<cr>", "tabstop 2"},
-		y = { "<cmd>set tabstop=4<cr><cmd>set shiftwidth=0<cr><cmd>set noet<cr>", "tabstop 4"},
-		z = { "<cmd>set tabstop=8<cr><cmd>set shiftwidth=0<cr><cmd>set noet<cr>", "tabstop 8"},
-		i = { "<cmd>IBLToggle<cr>", "Indent Lines" },
-		c = { function() vim.o.cursorline = not vim.o.cursorline end, "Cursorline" },
-		v = { function()
-			vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
-			print("Diagnostic virtual text: " .. (vim.diagnostic.config().virtual_text and "ON" or "OFF"))
-			end, "Diagnostic virtual text" },
-		s = { function()
-			vim.diagnostic.config({ signs = not vim.diagnostic.config().signs })
-			print("Diagnostic signs: " .. (vim.diagnostic.config().signs and "on" or "off"))
-			end, "Diagnostic signs" },
-		u = { function()
-			vim.diagnostic.config({ underline = not vim.diagnostic.config().underline })
-			print("Diagnostic underline: " .. (vim.diagnostic.config().underline and "on" or "off"))
-			end, "Diagnostic underline" },
-		w = { function() vim.o.list = not vim.o.list end, "Whitespace On/Off" },
-		W = { function()
-			if vim.o.listchars == "space:⋅,tab:› ,trail:,nbsp:+" then
-				vim.o.listchars = "tab:  ,trail:,nbsp:+,lead:"
-				print("Show leading / trailing space")
-			else
-				vim.o.listchars = "space:⋅,tab:› ,trail:,nbsp:+"
-				print("Show all whitespace")
-			end
-		end, "Whitespace Modes" },
-		m = { function ()
-			if vim.g.minimap_global_toggle then
-				vim.g.minimap_global_toggle = false
-				require('mini.map').close()
-			else
-				vim.g.minimap_global_toggle = true
-				---@diagnostic disable-next-line: inject-field
-				vim.b.minimap_disable = false
-				require('mini.map').open()
-			end
-		end, 'Minimap' },
-		b = { function ()
-			if vim.opt.background:get() == 'dark' then
-				vim.opt.background = 'light'
-				print('Light background')
-			else
-				vim.opt.background = 'dark'
-				print('Dark background')
-			end
-		end, 'Light / Dark Switch' },
-		t = { function ()
-			if vim.g.transparent_bg_toggle then
-				vim.g.transparent_bg_toggle = false
-				print('Transparent BG OFF')
-			else
-				vim.g.transparent_bg_toggle = true
-				print('Transparent BG ON')
-			end
-		end, 'Transparent BG' },
-		T = { function ()
-			if vim.g.transparent_floats_toggle then
-				vim.g.transparent_floats_toggle = false
-				print('Transparent floats OFF')
-			else
-				vim.g.transparent_floats_toggle = true
-				print('Transparent floats ON')
-			end
-		end, 'Transparent Floats' },
-		e = { "<cmd>vert topleft split<cr><cmd>vert res 30<cr><cmd>ene<cr><C-w>l", "Pad left" }
-	},
-	l = {
-		name = "LSP",
-		-- INFO: more in ./lsp-setup.lua
-		d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
-		f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
-		j = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic ]d", },
-		k = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic [d", },
-		c = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-		q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-		r = { "<cmd>Telescope lsp_references<cr>", "References [gr]" },
-		u = { "<cmd>LspRestart<cr>", "Restart LSP" },
-		e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
-	},
-	-- T = {
-	-- 	name = 'lsp config help',
-	-- 	i = { "<cmd>LspInfo<cr>", "Info" },
-	-- 	I = { "<cmd>Mason<cr>", "Mason Info" },
-	-- },
-}, { prefix = '<leader>' })
+-- 	u = {
+-- 		name = "UI",
+	{"<leader>ux", "<cmd>set tabstop=2<cr><cmd>set shiftwidth=0<cr><cmd>set noet<cr>", desc="Tabstop 2"},
+	{"<leader>uy", "<cmd>set tabstop=4<cr><cmd>set shiftwidth=0<cr><cmd>set noet<cr>", desc="Tabstop 4"},
+	{"<leader>uz", "<cmd>set tabstop=8<cr><cmd>set shiftwidth=0<cr><cmd>set noet<cr>", desc="Tabstop 8"},
+	{"<leader>ui", "<cmd>IBLToggle<cr>", desc="Indent Lines"},
+	{"<leader>uc", function() vim.o.cursorline = not vim.o.cursorline end, desc="Cursorline"},
+	{"<leader>ue", "<cmd>vert topleft split<cr><cmd>vert res 30<cr><cmd>ene<cr><C-w>l", desc="Pad left" },
+-- 		v = { function()
+-- 			vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
+-- 			print("Diagnostic virtual text: " .. (vim.diagnostic.config().virtual_text and "ON" or "OFF"))
+-- 			end, "Diagnostic virtual text" },
+-- 		s = { function()
+-- 			vim.diagnostic.config({ signs = not vim.diagnostic.config().signs })
+-- 			print("Diagnostic signs: " .. (vim.diagnostic.config().signs and "on" or "off"))
+-- 			end, "Diagnostic signs" },
+-- 		u = { function()
+-- 			vim.diagnostic.config({ underline = not vim.diagnostic.config().underline })
+-- 			print("Diagnostic underline: " .. (vim.diagnostic.config().underline and "on" or "off"))
+-- 			end, "Diagnostic underline" },
+-- 		w = { function() vim.o.list = not vim.o.list end, "Whitespace On/Off" },
+-- 		W = { function()
+-- 			if vim.o.listchars == "space:⋅,tab:› ,trail:,nbsp:+" then
+-- 				vim.o.listchars = "tab:  ,trail:,nbsp:+,lead:"
+-- 				print("Show leading / trailing space")
+-- 			else
+-- 				vim.o.listchars = "space:⋅,tab:› ,trail:,nbsp:+"
+-- 				print("Show all whitespace")
+-- 			end
+-- 		end, "Whitespace Modes" },
+-- 		m = { function ()
+-- 			if vim.g.minimap_global_toggle then
+-- 				vim.g.minimap_global_toggle = false
+-- 				require('mini.map').close()
+-- 			else
+-- 				vim.g.minimap_global_toggle = true
+-- 				---@diagnostic disable-next-line: inject-field
+-- 				vim.b.minimap_disable = false
+-- 				require('mini.map').open()
+-- 			end
+-- 		end, 'Minimap' },
+-- 		b = { function ()
+-- 			if vim.opt.background:get() == 'dark' then
+-- 				vim.opt.background = 'light'
+-- 				print('Light background')
+-- 			else
+-- 				vim.opt.background = 'dark'
+-- 				print('Dark background')
+-- 			end
+-- 		end, 'Light / Dark Switch' },
+-- 		t = { function ()
+-- 			if vim.g.transparent_bg_toggle then
+-- 				vim.g.transparent_bg_toggle = false
+-- 				print('Transparent BG OFF')
+-- 			else
+-- 				vim.g.transparent_bg_toggle = true
+-- 				print('Transparent BG ON')
+-- 			end
+-- 		end, 'Transparent BG' },
+-- 		T = { function ()
+-- 			if vim.g.transparent_floats_toggle then
+-- 				vim.g.transparent_floats_toggle = false
+-- 				print('Transparent floats OFF')
+-- 			else
+-- 				vim.g.transparent_floats_toggle = true
+-- 				print('Transparent floats ON')
+-- 			end
+-- 		end, 'Transparent Floats' },
+-- 	},
+-- 	l = {
+-- 		name = "LSP",
+-- 		-- INFO: more in ./lsp-setup.lua
+-- 		d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+-- 		f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
+-- 		j = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic ]d", },
+-- 		k = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic [d", },
+-- 		c = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+-- 		q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+-- 		r = { "<cmd>Telescope lsp_references<cr>", "References [gr]" },
+-- 		u = { "<cmd>LspRestart<cr>", "Restart LSP" },
+-- 		e = { "<cmd>Telescope quickfix<cr>", "Telescope Quickfix" },
+-- 	},
+-- 	-- T = {
+-- 	-- 	name = 'lsp config help',
+-- 	-- 	i = { "<cmd>LspInfo<cr>", "Info" },
+-- 	-- 	I = { "<cmd>Mason<cr>", "Mason Info" },
+-- 	-- },
+-- }, { prefix = '<leader>' })
 
 
--- [[ SHOW GROUP HINTS ]]
--- document existing key chains
-require('which-key').register {
-	['<leader>gh'] = { name = 'Hunk', _ = 'which_key_ignore' },
-	['<leader>gt'] = { name = 'Toggle', _ = 'which_key_ignore' },
-	['<leader>lw'] = { name = 'Workspace', _ = 'which_key_ignore' },
-}
--- register which-key VISUAL mode
--- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({
-	['<leader>'] = { name = 'VISUAL <leader>' },
-	['<leader>g'] = { 'Git' },
-	['<leader>gh'] = { 'Hunk' },
-}, { mode = 'v' })
 
+
+
+	-- [[ SHOW GROUP HINTS ]]
+	-- document existing key chains
+	{ '<leader>gh', group = 'Hunk' },
+	{ '<leader>gt', group = 'Toggle' },
+	{ '<leader>lw', group = 'Workspace' },
+	-- register which-key VISUAL mode
+	-- required for visual <leader>hs (hunk stage) to work
+	{ '<leader>', group = 'VISUAL <leader>', mode = 'v' },
+	{ '<leader>g', group = 'Git', mode = 'v' },
+	{ '<leader>gh', group = 'Hunk', mode = 'v' },
+})
 
 
 vim.keymap.set('i', '<C-a>', '<Esc>ggVG', { silent = true })
