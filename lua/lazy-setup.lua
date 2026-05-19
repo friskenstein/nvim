c90d0024eded3180e57eb56e459f8d66cc5c2b14
Johan Frisk <johan@friskenstein.com>
2026-05-19 10:03:38 +0300

    fix: treesitter duplicate config

--- lua/lazy-setup.lua
+++ lua/lazy-setup.lua
@@ -805,31 +805,94 @@ require('lazy').setup({
 		},
 	},
 
-{ -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
+	{
+		'nvim-treesitter/nvim-treesitter',
+		build = ':TSUpdate',
+		main = 'nvim-treesitter.configs',
+		opts = {
+			ensure_installed = {
+				'bash',
+				'c',
+				'cpp',
+				'diff',
+				'go',
+				'html',
+				'javascript',
+				'lua',
+				'luadoc',
+				'markdown',
+				'markdown_inline',
+				'python',
+				'query',
+				'rust',
+				'tsx',
+				'typescript',
+				'vim',
+				'vimdoc',
+			},
+			auto_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { 'ruby' },
+			},
+			indent = {
+				enable = true,
+				disable = { 'ruby' },
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = '<c-space>',
+					node_incremental = '<c-space>',
+					scope_incremental = '<c-s>',
+					node_decremental = '<M-space>',
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						['aa'] = '@parameter.outer',
+						['ia'] = '@parameter.inner',
+						['af'] = '@function.outer',
+						['if'] = '@function.inner',
+						['ac'] = '@class.outer',
+						['ic'] = '@class.inner',
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						[']m'] = '@function.outer',
+						[']c'] = '@class.outer',
+					},
+					goto_next_end = {
+						[']M'] = '@function.outer',
+						[']C'] = '@class.outer',
+					},
+					goto_previous_start = {
+						['[m'] = '@function.outer',
+						['[c'] = '@class.outer',
+					},
+					goto_previous_end = {
+						['[M'] = '@function.outer',
+						['[C'] = '@class.outer',
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						['<leader>lSp'] = '@parameter.inner',
+					},
+					swap_previous = {
+						['<leader>lSP'] = '@parameter.inner',
+					},
+				},
+			},
+		},
+	},
 
 
 	-- { import = 'colors' },
--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -3,7 +3,9 @@
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
-require('mason').setup()
+require('mason').setup({
+	PATH="append",
+})
 require('mason-lspconfig').setup()
 
 -- Enable the following language servers
